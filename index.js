74a01dabffa08bb7d2f49305826772482fb89beb
GiedreKite <giedre.narvilaite@gmail.com>
2024-08-27 22:53:19 +0300

    up

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import { pageAbout } from './pages/pageAbout.js';
 const app = express();
 const port = 5018;
 
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -15,6 +16,20 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     return res.send(pageAbout());
 });
+
+app.get('/create-account', (req, res) => {
+    return res.send(pageCreateAccount());
+});
+
+app.get('/accounts', (req, res) => {
+    return res.send(pageViewAllAccounts());
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+  })
+
 
 // app.get('/css/main.css', (req, res) => {
 //     return res
